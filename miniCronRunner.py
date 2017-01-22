21fbe5befe38c1c738c79c023329920a223c75bd
abadied <abadi.eden@gmail.com>
2017-01-22 15:48:50 +0200

    cronrunner started

--- miniCronRunner.py
+++ miniCronRunner.py
@@ -1,13 +1,19 @@
 import hotelWorker
 import sqlite3
 import os
-
+import time
 
 databaseexisted = os.path.isfile('cronhoteldb.db')
 dbcon = sqlite3.connect('cronhoteldb.db')
 
 with dbcon:
     cursor = dbcon.cursor()
-    cursor.execute("SELECT TaskId FROM TaskTimes WHERE NumTimes != (?)",(0))
+   # print str(tasks[0][0]) #Test
+    while databaseexisted:
+        cursor.execute("SELECT * FROM TaskTimes WHERE NumTimes != 0")
+        tasks = cursor.fetchall()
+        if len(tasks) <= 0:
+            break
 
-    if databaseexisted:
+
+
